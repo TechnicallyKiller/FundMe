c98550b6eb7e0ec40ab688c1c38b692e19ec94db
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2024-09-05 14:49:29 +0530

    chore: forge init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,23 +1,14 @@
-// SPDX-License-Identifier: MIT
- pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
+contract Counter {
+    uint256 public number;
 
-contract SimpleStorage {
-    uint256 public favoriteNum;
-    uint256[] public listFavNum;
-
-    struct Person {
-        uint256 favoriteNum;
-        string name;
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
     }
 
-    Person[] public listOfPeeps;
-    mapping(string => uint256) public nameToFav;
-
-    // Store a new favorite number
-    function store(uint256 _favoriteNum) public {
-        favoriteNum = _favoriteNum;
+    function increment() public {
+        number++;
     }
-
-   
 }
