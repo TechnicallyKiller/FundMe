e0150eede766515e5607843b7f79af2fb11a257c
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2024-10-11 19:47:36 +0530

    fixing

--- src/fundme.sol
+++ src/fundme.sol
@@ -1,48 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
-
-import {AggregatorV3Interface} from "node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import {fileConv} from "./FileConv.sol";
-
+import { AggregatorV3Interface } from "node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { fileConv} from "./FileConv.sol";
 contract FundMe {
-    uint256 public myValue = 1;
+ uint256 public myValue=1;
+ using fileConv for uint256;
+ 
+ address[] public funds;
+ mapping(uint256=>address) public AmountToAddress;
+ mapping (address funder => uint256 funded) addressToAmount;
+  // Function to get the price of Ethereum in USD
+ address public owner;
+ AggregatorV3Interface private s_pricefeed;
 
-    using fileConv for uint256;
+ constructor(address  pricefeed){
 
-    address[] public funds;
-    mapping(uint256 => address) public AmountToAddress;
-    mapping(address funder => uint256 funded) addressToAmount;
-    // Function to get the price of Ethereum in USD
-    address public owner;
-    AggregatorV3Interface private s_pricefeed;
+ owner=msg.sender;
+ s_pricefeed= AggregatorV3Interface(pricefeed);
 
-    constructor(address pricefeed) {
-        owner = msg.sender;
-        s_pricefeed = AggregatorV3Interface(s_pricefeed);
-    }
 
-    uint256 public minUsd = 5e18;
-
-    function getPricefeed() public view returns (uint256) {
+ }
+uint256 public minUsd=5e18;
+    function getPricefeed() public view returns(uint256){
         return s_pricefeed.version();
-    }
 
-    function fund() public payable {
+    }
+    function fund() public payable   {
+        
         require(msg.value.getConversionRate(s_pricefeed) >= minUsd, "You need to spend more ETH!");
         funds.push(msg.sender);
-        addressToAmount[msg.sender] = addressToAmount[msg.sender] + msg.value;
+        addressToAmount[msg.sender]=addressToAmount[msg.sender]+msg.value;
     }
-
     function tinyTip() public payable {
-        require(msg.value < 1e18, "not small enough");
+        require(msg.value<1e18, "not small enough");
     }
-
     function withdraw() public onlyOwner {
-        for (uint256 funIndex = 0; funIndex < funds.length; funIndex++) {
+        
+        for (uint funIndex =0 ; funIndex< funds.length;funIndex++){
             address funder = funds[funIndex];
-            addressToAmount[funder] = 0;
+            addressToAmount[funder]=0;
         }
-        funds = new address[](0);
+        funds= new address[](0);
         // transfer
         // send
         //call
@@ -52,24 +50,27 @@ contract FundMe {
         // //send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess,"failed");
-
+    
         //  //will give boolean response if trans passed or not above 2300gas
         // //call
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess ,) =payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, " Call Failed");
-    }
+        
 
-    modifier onlyOwner() {
+    }
+    modifier onlyOwner(){
         require(msg.sender == owner, "sender is not owner");
         _;
     }
     // what happens if someone sends this contract ETH without calling the fund function
-
-    receive() external payable {
+    receive() external payable { 
         fund();
     }
-
     fallback() external payable {
         fund();
-    }
+     }
+
+
+
+    
 }
